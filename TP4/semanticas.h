ccbca52cf56413a137cf852b567ae2586fcf27b0
Javier Ouret <43023124+jaouret@users.noreply.github.com>
2023-09-25 10:38:17 -0300

    Add files via upload

---
+++ TP4/semanticas.h
@@ -0,0 +1,21 @@
+/* tipos de tokens */
+#define UNDEF 0
+#define INT_TYPE 1
+#define REAL_TYPE 2
+#define CHAR_TYPE 3
+#define ARRAY_TYPE 4
+#define POINTER_TYPE 5
+#define FUNCTION_TYPE 6
+
+/* tipos de operadores */
+#define NONE 0		// para comprobar tipos solamente - asignación, parámetro
+#define ARITHM_OP 1 // ADDOP, MULOP, DIVOP (+, -, *, /)
+#define INCR_OP 2   // INCR (++, --)
+#define BOOL_OP 3   // OROP, ANDOP (||, &&)
+#define NOT_OP 4    // NOTOP (!)
+#define REL_OP 5    // RELOP (>, <, >=, <=)
+#define EQU_OP 6    // EQUOP (==, !=)
+
+// Declaraciones de funciones
+int get_result_type (int type_1, int type_2, int op_type); /* verificación de tipo y tipo de resultado */
+void type_error(int type_1, int type_2, int op_type);      /* imprimir error de tipo */
